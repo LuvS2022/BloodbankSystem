d5f8356f3663043fad1c24aba92778f135ae96e6
Luv Sahu <112633451+LuvS2022@users.noreply.github.com>
2024-04-11 10:22:42 +0530

    Add files via upload

---
+++ bloodbank.sql
@@ -0,0 +1,193 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 11, 2024 at 05:48 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bloodbank`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bloodinfo`
+--
+
+CREATE TABLE `bloodinfo` (
+  `bid` int(11) NOT NULL,
+  `hid` int(11) NOT NULL,
+  `bg` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `bloodinfo`
+--
+
+INSERT INTO `bloodinfo` (`bid`, `hid`, `bg`) VALUES
+(1, 1, 'B+'),
+(2, 2, 'A+'),
+(3, 3, 'A-'),
+(4, 4, 'A-');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bloodrequest`
+--
+
+CREATE TABLE `bloodrequest` (
+  `reqid` int(11) NOT NULL,
+  `hid` int(11) NOT NULL,
+  `rid` int(11) NOT NULL,
+  `bg` varchar(11) NOT NULL,
+  `status` varchar(100) NOT NULL DEFAULT 'Pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `bloodrequest`
+--
+
+INSERT INTO `bloodrequest` (`reqid`, `hid`, `rid`, `bg`, `status`) VALUES
+(1, 1, 1, 'B+', 'Pending'),
+(3, 3, 3, 'A-', 'Pending'),
+(4, 4, 3, 'A-', 'Pending'),
+(5, 1, 4, 'B+', 'Pending');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hospitals`
+--
+
+CREATE TABLE `hospitals` (
+  `id` int(11) NOT NULL,
+  `hname` varchar(100) NOT NULL,
+  `hemail` varchar(100) NOT NULL,
+  `hpassword` varchar(100) NOT NULL,
+  `hphone` varchar(100) NOT NULL,
+  `hcity` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `hospitals`
+--
+
+INSERT INTO `hospitals` (`id`, `hname`, `hemail`, `hpassword`, `hphone`, `hcity`) VALUES
+(1, 'Arora hospital', 'arora@gmail.com', 'arora@123', '7899876545', 'jhansi Nagra'),
+(2, 'Vinayak hospital', 'Vinayak@gmail.com', '123456', '6549877895', 'jhansi city'),
+(3, 'kapoor Hostipal', 'kapoor@gmail.com', '123456', '7845128956', 'Jhansi elit'),
+(4, 'Jhansi Orthopaedic Hospital', 'Orthopaedic@gmail.com', '123456', '1593579635', 'jhansi medical');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `receivers`
+--
+
+CREATE TABLE `receivers` (
+  `id` int(11) NOT NULL,
+  `rname` varchar(100) NOT NULL,
+  `remail` varchar(100) NOT NULL,
+  `rpassword` varchar(100) NOT NULL,
+  `rphone` varchar(100) NOT NULL,
+  `rbg` varchar(10) NOT NULL,
+  `rcity` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `receivers`
+--
+
+INSERT INTO `receivers` (`id`, `rname`, `remail`, `rpassword`, `rphone`, `rbg`, `rcity`) VALUES
+(2, 'rec two', 'rec2@gmail.com', '123456', '9633698526', 'A+', 'jhansi'),
+(3, 'rev three', 'revthree@gmail.com', '123456', '6544568526', 'B-', 'noida'),
+(4, 'Rec twotwo', 'rev22@gmail.com', '123456', '5556665558', 'AB-', 'Nagra');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bloodinfo`
+--
+ALTER TABLE `bloodinfo`
+  ADD PRIMARY KEY (`bid`),
+  ADD KEY `hid` (`hid`);
+
+--
+-- Indexes for table `bloodrequest`
+--
+ALTER TABLE `bloodrequest`
+  ADD PRIMARY KEY (`reqid`),
+  ADD KEY `hid` (`hid`);
+
+--
+-- Indexes for table `hospitals`
+--
+ALTER TABLE `hospitals`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `hemail` (`hemail`);
+
+--
+-- Indexes for table `receivers`
+--
+ALTER TABLE `receivers`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `remail` (`remail`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bloodinfo`
+--
+ALTER TABLE `bloodinfo`
+  MODIFY `bid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `bloodrequest`
+--
+ALTER TABLE `bloodrequest`
+  MODIFY `reqid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `hospitals`
+--
+ALTER TABLE `hospitals`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `receivers`
+--
+ALTER TABLE `receivers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bloodinfo`
+--
+ALTER TABLE `bloodinfo`
+  ADD CONSTRAINT `bloodinfo_ibfk_1` FOREIGN KEY (`hid`) REFERENCES `hospitals` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
